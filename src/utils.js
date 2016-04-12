3605ad52b208a0c80913a5f17279f14aa2b5aab7
namlook <nicolas.clairon@elkorado.com>
2016-04-12 17:35:41 +0200

    add importDir utility

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,16 @@
 
 import _ from 'lodash';
+import requireDir from 'require-dir';
 
 export const pascalCase = (string) => _.startCase(string).split(' ').join('');
+
+export const importDir = (string) => {
+    const _resources = requireDir(string);
+    return Object.keys(_resources).reduce((acc, resourceName) => {
+        /** hack for es6 and require-dir **/
+        const resource = _resources[resourceName].__esModule
+            ? _resources[resourceName].default
+            : _resources[resourceName];
+        return Object.assign({}, acc, { [resourceName]: resource });
+    }, {});
+};
